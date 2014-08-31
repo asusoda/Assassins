344f332b37107e5c422121dd934f38b4c7f82d79
Jack Ketcham <kcajketcham@gmail.com>
2014-08-31 15:33:14 -0700

    New controllers for creating, editing, viewing

--- client/app/game/game.controller.js
+++ client/app/game/game.controller.js
@@ -1,19 +1,21 @@
 'use strict';
 
 angular.module('assassinsApp')
-  .controller('GameCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('GameExploreCtrl', function ($scope, Games, Auth) {
+    $scope.games = Games.getGames().$object;
+
+    $scope.isLoggedIn = Auth.isLoggedIn;
   });
 
 angular.module('assassinsApp')
-  .controller('GameCreateCtrl', function ($scope, Games) {
+  .controller('GameCreateCtrl', function ($scope, $state, Games) {
     $scope.formData = {};
-    $scope.formData.game = {};
 
     $scope.create = function() {
       Games.create($scope.formData).then(function(res) {
         if(res && !res.error) {
-
+          console.log(res);
+          $state.go('game.page', { id: res._id });
         } else {
           console.log('Oops, there was an error: ' + res.error);
         }
@@ -22,6 +24,27 @@ angular.module('assassinsApp')
   });
 
 angular.module('assassinsApp')
-  .controller('GamePageCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('GamePageCtrl', function ($scope, Games, game) {
+    $scope.game = game;
+  });
+
+angular.module('assassinsApp')
+  .controller('GameEditCtrl', function ($scope, $stateParams, Games, game) {
+    $scope.game = game;
+
+    $scope.formData = {};
+    $scope.formData.title = game.title;
+    $scope.formData.description = game.description;
+
+    $scope.edit = function() {
+      Games.edit($scope.game._id, $scope.formData).then(function(res) {
+        if(res && !res.error) {
+          console.log(res);
+          $scope.game.title = $scope.formData.title;
+          $scope.game.description = $scope.formData.description;
+        } else {
+          console.log('Oops, there was an error: ' + res.error);
+        }
+      });
+    }
   });
